89601955a5963a06d5ea7295d62844f7a49f42d6
Gleb <mega.kiselyov@mail.ru>
2025-01-10 16:50:19 +0300

    Add based functions to menu

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,26 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu';
 
 export class ContextMenu extends Menu {
+   constructor(selector) {
+     super(selector);
+     this.open();
+     this.close();
+   }
+  open() {
+    document.addEventListener('contextmenu', (event) => {
+      event.preventDefault();
 
+      const menu = document.querySelector('.menu');
+      menu.classList.add('open');
+      menu.style.left = `${event.pageX}px`;
+      menu.style.top = `${event.pageY}px`;
+    });
+  }
+
+  close() {
+    document.addEventListener('click', () => {
+        const menu = document.querySelector('.menu');
+        menu.classList.remove('open');
+      });
+  }
 }
