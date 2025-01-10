f391d5c1bfd4a18d3683cc950b21388f90b9a3cc
Gleb <mega.kiselyov@mail.ru>
2025-01-10 17:35:05 +0300

    Complete function add()

--- src/menu.js
+++ src/menu.js
@@ -1,26 +1,42 @@
 import { Menu } from './core/menu';
 
 export class ContextMenu extends Menu {
+  #menu
+
    constructor(selector) {
      super(selector);
-     this.open();
-     this.close();
+     if(document.querySelector('.menu-item')){
+      this.open();
+      this.close();
+
+      this.#menu = document.querySelector('.menu');
+     }
    }
+
   open() {
     document.addEventListener('contextmenu', (event) => {
       event.preventDefault();
 
-      const menu = document.querySelector('.menu');
-      menu.classList.add('open');
-      menu.style.left = `${event.pageX}px`;
-      menu.style.top = `${event.pageY}px`;
+      this.#menu.classList.add('open');
+      this.#menu.style.left = `${event.pageX}px`;
+      this.#menu.style.top = `${event.pageY}px`;
     });
   }
 
   close() {
     document.addEventListener('click', () => {
-        const menu = document.querySelector('.menu');
-        menu.classList.remove('open');
+        this.#menu.classList.remove('open');
       });
+  }
+
+  add(module){
+    const menuItem = document.createElement('li')
+
+    menuItem.className = 'menu-item'
+    menuItem.innerText = module.name
+
+    this.#menu.append(menuItem)
+
+    menuItem.addEventListener('click', module.trigger)
   }
 }
