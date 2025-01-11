f548af9785a41f86bbb5524a940ad46a6674b178
helga2lip <helga2lip@gmail.com>
2025-01-11 14:35:29 +0300

    added shape module in app.js and increased context menu size

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import './styles.css'
 import { ContextMenu } from './menu'
 import { Timer } from './modules/timer.module';
+import { ShapeModule } from './modules/shape-module/shape.module';
 
 const contextMenu = new ContextMenu();
 
-const timer = new Timer('timer', 'Таймер отсчета')  
+const timer = new Timer('timer', 'Таймер отсчета')
+const randomShape = new ShapeModule('randomShape', 'Случайная фигура')  
 contextMenu.add(timer)
+contextMenu.add(randomShape)
 
